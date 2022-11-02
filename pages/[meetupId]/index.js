39e4c009f0f70ac1d7e65e929f45742deda9101c
agbomonica <agbomonica.am@gmail.com>
2022-11-02 15:51:48 +0100

    added fallback for dynamic meetup route

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -33,7 +33,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => {
       return { params: { meetupId: meetup._id.toString() } };
     }),
